e9f6c645793759c2da5af478d3b7135287a56da8
Miki Hau <mikihau@yahoo.com>
2021-09-25 17:54:46 -0700

    secret setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!")
+import { Client } from '@notionhq/client';
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN
+});
+(async () => {
+  const response = await notion.search({
+    query: 'ACGN', // name of the DB
+  });
+  console.log(response.results[0].id);
+})();
