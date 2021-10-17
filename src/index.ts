8ffd3e4fc48e961887c3b5a0d20e35d39c8f0b43
Miki Hau <mikihau@yahoo.com>
2021-10-17 06:52:49 -0700

    easier testing setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-//import { createLogger, format, transports } from 'winston';
 import { retrieve as retrieveDouban } from './retrievers/douban.js';
 import { update as updateNotion } from './responders/notion.js';
 import { update as tootOnMastodon } from './responders/mastodon.js';
 import { ConsumptionAttributes, transform } from './transformers/transformer.js';
+import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import winston from 'winston';
 
 export const logger = winston.createLogger({
@@ -16,10 +18,6 @@ export const logger = winston.createLogger({
     })
   ]
 });
-
-const input = getInputFromEnvVars();
-logger.debug(input);
-(async () => processEvent(input))();
 
 export interface ConsumptionInput {
   database: string;
@@ -69,6 +67,21 @@ export interface ResponderExecutionResult {
   message?: string;
 }
 
+let isProd = false;
+if (process.env.ENVIRONMENT === "production") {
+  isProd = true;
+  const input = getInputFromEnvVars();
+  logger.debug(input);
+  (async () => processEvent(input))();
+} else {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  var testEvents = JSON.parse(readFileSync(`${__dirname}/../test_data/events.json`, 'utf8'));
+  for (let testEvent of testEvents) {
+    (async () => processEvent(testEvent))();
+  }
+}
+
 function getInputFromEnvVars(): ConsumptionInput {
   const database = process.env.INPUT_DATABASE;
   logger.info(`database: ${database}`);
@@ -94,7 +107,7 @@ function getInputFromEnvVars(): ConsumptionInput {
   }
 }
 
-async function processEvent(input: ConsumptionInput) {
+async function processEvent<T extends ConsumptionInput>(input: T) {
   const retriever = getRetriever(input);
   if (!retriever) {
     throw `No metadata retrievers implemented for link: ${input.origin}`;
@@ -110,13 +123,16 @@ async function processEvent(input: ConsumptionInput) {
   logger.info(`Transformed input: ${JSON.stringify(attributes, null, 2)}`);
 
   const result: ResponderExecutionResult[] = [];
-  for (let responder of getResponders(attributes)) {
-    let response = await responder(attributes);
-    logger.info(`Response from responder ${responder.name}: ${JSON.stringify(response, null, 2)}`);
-    result.push(response);
+  if (isProd) {
+    for (let responder of getResponders(attributes)) {
+      let response = await responder(attributes);
+      logger.info(`Response from responder ${responder.name}: ${JSON.stringify(response, null, 2)}`);
+      result.push(response);
+    }
+  } else {
+    result.push({"success": true});
   }
 
-  result.push({success: false});
   // exit with non-0 exit code if any of the reponders fail
   if (result.filter((entry) => !entry.success).length > 0) {
     process.exit(1);
